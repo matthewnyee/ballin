f3c5913b947ba3f5bebde607c66aca873a627915
Matt Yee <mattyee@Matts-MacBook-Pro.local>
2015-09-18 12:49:21 -0700

    Use CSS/Bootstrap to look better, add about page, redirect create action to edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    next_game
+    @next_game_day_name = next_game_day_name
   end
 
   def new
@@ -13,11 +15,13 @@ class UsersController < ApplicationController
     @user = User.new
     @user.name = params[:user][:name]
     @user.save
-    redirect_to users_url
+    redirect_to edit_user_url(@user.id)
   end
 
   def edit
     @user = User.find(params[:id])
+    next_game
+    @next_game_day_name = next_game_day_name
   end
 
   def update
@@ -35,12 +39,60 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url
+  end
 
-#Don't need user_params anymore, just use params[:user][:status] or params[:user][:name]
-  # private
-  #
-  #   def user_params
-  #     params.require(:user).permit(:name, :status)
-  #   end
+  def about
+  end
+
+
+  private
+
+    ##Don't need user_params anymore, just use params[:user][:status] or params[:user][:name]
+    #   def user_params
+    #     params.require(:user).permit(:name, :status)
+    #   end
+
+  def next_game
+    if Date.today.wday >= 1 && Date.today.wday <= 3
+      @next_game = next_wednesday
+    else
+      @next_game = next_sunday
+    end
+  end
+
+def next_wednesday
+  x = Date.today
+  x += 1 until x.wday > 2 && x.wday < 4#for some reason, can't just use equal sign...
+  x
+end
+
+def next_sunday
+  x = Date.today
+  x += 1 until x.wday <=0 #for some reason, can't just use equal sign
+  x
+end
+
+def today?
+  return true if @next_game == Date.today
+end
+
+def tomorrow?
+  return true if @next_game == Date.today + 1
+end
+
+def next_game_day_name
+  if today?
+    'today'
+  elsif tomorrow?
+    'tomorrow'
+  else
+    "on #{Date::DAYNAMES[@next_game.wday]}"
+  end
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :users
 
   # You can have the root of your site routed with "root"
-   root 'users#index'
+  root 'users#index'
+
+  get '/about' => 'users#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
